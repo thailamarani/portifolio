5feecfb49881a76019a019a5a19b90b0868666ec
Thaila Marani <thaila_marani@hotmail.com>
2023-10-17 21:02:19 -0300

    Portifólio pessoal

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# portifolio
+# Portfólio Angular
+
+Este é o meu portfólio pessoal construído usando o Angular. Ele apresenta minha experiência, projetos e habilidades de desenvolvimento web.
+
+## Demonstração
+
+Inclua um link ou captura de tela que mostre o seu portfólio em ação.
+
+## Recursos
+
+- Lista de projetos
+- Descrições detalhadas de projetos
+- Informações de contato
+- Experiência e habilidades
+
+Projeto por: Thaila Marani
+https://github.com/thailamarani/portifolio
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,20 @@
+export const dataFake = [
+  {
+    id: "2",
+    title: "Educação",
+    description: "• Graduação tecnológica em Análise e Desenvolvimento de Sistemas pela Universidade de Marília (cursando)\n • Certificado de conclusão do Bootcamp Santander Fullstack Java + Angular (Outubro/2023)\n • Formada no Bootcamp Labenu Fullstack em Agosto/2023, com conhecimento nas tecnologias HTML, CSS, Javascript, React, Node.JS, Typescript e SQL e desenvolvimento de projetos (Agosto/2023)\n • Graduada em Administração pelo Centro Universitário Toledo Presidente Prudente (Novembro/2015)\n • Pós graduada em Controladoria e Gestão Financeira pelo Centro Universitário Presidente Prudente (Novembro/2013)\n\n COMPLEMENTAR:\n • Cursando Python Impressionador pela Hashtag Treinamentos\n • Curso de Inglês na Delfin English School - Dublin/Irlanda",
+    photoCover: "https://faculdadesenacpe.edu.br/v19/wp-content/uploads/2021/06/Estudantes-de-Analise-e-Desenvolvimento-de-Sistemas-participam-do-ProjetAi-1536x1016.jpg"
+  },
+  {
+    id: "3",
+    title: "Experiências profissionais",
+    description: "• Gerente Administrativa\n Restaurante Franchesco - Rancharia/SP (Out/2018 - Jul/2023)\n\n • Agente de Atendimento ao Cliente - Rancharia/SP\n Sicoob Credivale (Fev/2015 - Jan/2018)\n • Analista de Crédito - Presidente Prudente/SP\n Sicoob Credivale (Jan/2014 - Fev/2015) \n • Estagiária - Presidetne Prudente/SP\n Sicoob Credivale (Set/2012 - Dez/2013) \n\n • Estagiária\n Caixa Econômica Federal (Jan/2012 - Ago/2012) \n\n • Estagiária - Presidetne Prudente/SP\n Prefeitura Municipal de Rancharia/SP (Nov/2010 - Out/2011)",
+    photoCover: "https://blog.finofaro.com.br/wp-content/uploads/2018/10/dicas-para-crescimento-profissional.jpg"
+  },
+  {
+    id: "4",
+    title: "Meus Contatos",
+    description: "WhatsApp: (18) 99617-7667\nE-mail: thaila_marani@hotmail.com\n Quatá/SP",
+    photoCover: "https://festivalnezrouges38.fr/wp-content/uploads/2020/07/call-center.jpg"
+  }
+];
